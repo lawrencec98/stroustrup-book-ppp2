aa5638dd02169ad2db6d912a0b1de13a86ee21f6
Lawrence Catapang <lawrencemcatapang@gmail.com>
2024-02-01 23:39:16 +0000

    progress in chapter18 exercises

--- chapter18/exercise07.cpp
+++ chapter18/exercise07.cpp
@@ -5,13 +5,57 @@
 */
 
 #include <string>
+#include <iostream>
+
+
+int str_len(const char* s1) {
+
+    int count = 0;
+    while(*s1 != '\0') {
+        ++s1;
+        ++count;
+    }
+    return count;
+}
 
 
 char* cat_dot(const char* s1, const char* s2) {
+    
+    int size_s1 = str_len(s1);
+    int size_s2 = str_len(s2);
 
+    char* new_string = new char[size_s1 + size_s2 + 2];
+
+    for(int i = 0; i < size_s1; i++) {
+        new_string[i] = s1[i]; 
+    }
+
+    new_string[size_s1] = '.';
+
+    for(int i = 0; i < size_s2; i++) {
+        new_string[size_s1 + 1 + i] = s2[i];
+    }
+
+    new_string[size_s1 + size_s2 + 1] = '\0';
+
+    return new_string;
 }
 
 
 int main() {
+
+    const char s1[] = {"James"};
+    const char s2[] = {"Bond"};
+
+    char* s3 = cat_dot(s1,s2);
+    char* copy_of_s3 = s3;
     
+    while(*s3 != '\0') {
+        std::cout << *s3;
+        s3++;
+    }
+    std::cout << std::endl;
+
+    delete[] copy_of_s3;
+    return 0;
 }
